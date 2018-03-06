92f3d04751f82a4d3c9f84dfcd3a660647c17658
williambl <william@williambl.com>
2018-03-06 15:58:47 +0000

    Added a fluid script to the fluid object

---
+++ unity-fluid-float/Assets/Scripts/Fluid.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fluid : MonoBehaviour {
+
+    void Start () {
+		
+    }
+	
+    void Update () {
+		
+    }
+
+    public void OnTriggerEnter(Collider other) {
+       if (other.gameObject.GetComponent<Rigidbody>() == null)
+           return;
+    }
+}
